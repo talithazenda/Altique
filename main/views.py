fdc9ff3d1861492eaecdbd4267bf3c89913f60f7
talitha.zenda <talithazenda26@gmail.com>
2024-09-08 01:28:33 -0700

    nambahin nama app

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Product  # Import model Product
 
 def home(request):
     context = {
@@ -7,3 +8,11 @@ def home(request):
         'student_class': 'PBP A'
     }
     return render(request, 'home.html', context)
+
+
+def product_list(request):
+    products = Product.objects.all()  # Ambil semua produk
+    context = {
+        'products': products  # Kirim daftar produk ke template
+    }
+    return render(request, 'product_list.html', context)
