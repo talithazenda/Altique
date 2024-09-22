91eeaef24940d9c5e7247a095cedb51eded164e0
talitha.zenda <talithazenda26@gmail.com>
2024-09-22 06:12:01 -0700

    Alhamdulillah tugas 4 kelar yey!

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from main.views import show_json, show_json_by_id, show_main, show_xml, show_xml_by_id, add_item_barang, show_items
+from main.views import show_json, show_json_by_id, show_main, show_xml, show_xml_by_id, add_item_barang, show_items, register, login_user, logout_user
+
 
 app_name = 'main'
 
@@ -11,4 +12,7 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<uuid:id>/', show_xml_by_id, name='show_xml_by_id'),  # Changed to uuid
     path('json/<uuid:id>/', show_json_by_id, name='show_json_by_id'),  # Changed to uuid
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
